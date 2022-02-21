75855e3c127b1859c6212af70fa4148f15fb663a
zhaoxingming <zhaoxingming@nucarf.com>
2022-02-21 08:49:10 +0800

    chore: delete

--- packages/@olad/olad-lint-cli/src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import { sync } from "globby"
-import { join } from "path/posix"
-
-export const getCommandPaths = () => {
-    return sync('../commands/*.js', { cwd: __dirname, deep: 1 }).map(path => join(__dirname, path))
-}
