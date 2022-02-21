6276eea07e97bd95d5f6c0142c4528728039b3d4
zhaoxingming <zhaoxingming@nucarf.com>
2022-02-21 23:06:45 +0800

    chore: 文件名更改

---
+++ packages/eslint-config-olad/vue.js
@@ -0,0 +1,21 @@
+const vueRules = require("./rules/vue-common.js");
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:vue/essential",
+    "./base.js",
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+  },
+  plugins: ["vue", "prettier"],
+  rules: Object.assign({}, vueRules),
+};
