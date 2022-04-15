919f457e474fd1680e2d62eeea34ea559be85ae5
zhaoxingming <zhaoxingming@nucarf.com>
2022-04-15 16:49:52 +0800

    chore:

--- packages/eslint-config-vue2/index.js
+++ packages/eslint-config-vue2/index.js
@@ -5,7 +5,6 @@ module.exports = {
   parser: "vue-eslint-parser",
   extends: ["@olad/eslint-config-basic"],
   parserOptions: {
-    ecmaVersion: 2022,
     sourceType: "module",
   },
   plugins: ["vue"],
