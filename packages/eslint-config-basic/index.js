6ea171a1edeece232a609463143a4cdae3d8b02f
zhaoxingming <zhaoxingming@nucarf.com>
2022-05-03 22:17:04 +0800

    chore: 规则文件抽离

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -1,11 +1,12 @@
 /*
  * @Date: 2022-02-21 16:09:24
  * @LastEditors: zhaoxm
- * @LastEditTime: 2022-05-03 21:50:08
+ * @LastEditTime: 2022-05-03 22:15:13
  * @Description: 公共配置
  */
 
 const rules = require("./rules.js")
+const unicorn = require("./unicorn.js")
 
 module.exports = {
   env: {
@@ -15,19 +16,17 @@ module.exports = {
     es2021: true,
   },
   parserOptions: {
-    ecmaVersion: 2022,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-  extends:["plugin:unicorn/recommended"],
   globals: {
     document: "readonly",
     navigator: "readonly",
     window: "readonly",
   },
-  plugins: ["html", "promise", "import"],
+  plugins: ["html", "promise", "import", "unicorn"],
   ignorePatterns: [
     "*.min.*",
     "CHANGELOG.md",
@@ -47,22 +46,7 @@ module.exports = {
   ],
   rules: {
     ...rules,
-
-    // unicorn off 
-    // 强制对长度使用 === 0 来判断
-    "unicorn/explicit-length-check": "off",
-    // 强制文件夹命名规范,后续可以根据需求再开放出来
-    "unicorn/filename-case":"off",
-    // 复杂情况不使用 reduce 
-    "unicorn/no-array-reduce":"off",
-    // 不使用null复制
-    "unicorn/no-null": "off",
-    // 入参对象不进行设置默认值
-    "unicorn/no-object-as-default-parameter": "off",
-    // 不允许赋值this
-    "no-this-assignment":"off",
-    "unicorn/prefer-dom-node-text-content": "off",
-
+    ...unicorn,
     // 强制使用一致的参数名和顺序。
     "promise/param-names": "error",
     // 报告任何无效的导出，即同名的再导出
