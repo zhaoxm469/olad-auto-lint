85b7360b6884eede153b86ff7e1c98ea1692f352
zhaoxingming <zhaoxingming@nucarf.com>
2022-05-04 00:43:30 +0800

    chore: 规则更改

--- packages/eslint-config-vue3/index.js
+++ packages/eslint-config-vue3/index.js
@@ -7,33 +7,63 @@ module.exports = {
         parser: "@typescript-eslint/parser",
       },
       rules: {
-        "no-unused-vars": "off",
-        "no-undef": "off",
         "@typescript-eslint/no-unused-vars": "off",
+        // 强制文件命名规范,
+        "unicorn/filename-case":["error", {
+          "case": "kebabCase",
+        }],
+
+        "vue/html-self-closing": [
+          "error",
+          {
+            html: {
+              void: "never",
+              normal: "always",
+              component: "always",
+            },
+            svg: "always",
+            math: "always",
+          },
+        ],
+        "vue/no-multiple-slot-args": ["error"],
+        "vue/order-in-components": ["error"],
+        "vue/this-in-template": ["error"],
+        "vue/attribute-hyphenation": ["error"],
+        "vue/component-definition-name-casing": ["error"],
+        "vue/html-closing-bracket-newline": ["error"],
+        "vue/html-closing-bracket-spacing": ["error"],
+        "vue/html-end-tags": ["error"],
+        "vue/html-indent": ["error"],
+        "vue/html-quotes": ["error"],
+        "vue/multiline-html-element-content-newline": ["error"],
+        "vue/mustache-interpolation-spacing": ["error"],
+        "vue/no-multi-spaces": ["error"],
+        "vue/no-spaces-around-equal-signs-in-attribute": ["error"],
+        "vue/no-template-shadow": ["error"],
+        "vue/one-component-per-file": ["error"],
+        "vue/prop-name-casing": ["error"],
+        "vue/require-default-prop": ["error"],
+        "vue/require-explicit-emits": ["error"],
+        "vue/require-prop-types": ["error"],
+        "vue/singleline-html-element-content-newline": ["error"],
+        "vue/v-bind-style": ["error"],
+        "vue/v-on-style": ["error"],
+        "vue/v-slot-style": ["error"],
+        "vue/attributes-order": "error",
+        "vue/max-attributes-per-line": "off",
+        "vue/multi-word-component-names": "off",
+
+        // reactivity transform
+        "vue/no-setup-props-destructure": "off",
+
+        "vue/component-tags-order": [
+          "error",
+          {
+            order: ["script", "template", "style"],
+          },
+        ],
       },
     },
   ],
   extends: ["@olad/eslint-config-basic",  "@olad/eslint-config-ts", "plugin:vue/vue3-recommended"],
-  rules: {
-    // 强制文件命名规范,
-    "unicorn/filename-case":["error", {
-      "case": "kebabCase",
-    }],
-
-    "vue/max-attributes-per-line": "off",
-    "vue/no-v-html": "off",
-    "vue/require-prop-types": "off",
-    "vue/require-default-prop": "off",
-    "vue/multi-word-component-names": "off",
-
-    // reactivity transform
-    "vue/no-setup-props-destructure": "off",
-
-    "vue/component-tags-order": [
-      "error",
-      {
-        order: ["script", "template", "style"],
-      },
-    ],
-  },
 }
