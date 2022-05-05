1e070e5e4511b75b15aa3ba339a9223547a1da8b
zhaoxingming <zhaoxingming@nucarf.com>
2022-05-05 09:25:36 +0800

    feat: 完善upgrade命令

--- packages/lint-cli/src/commands/upgrade.ts
+++ packages/lint-cli/src/commands/upgrade.ts
@@ -1,12 +1,13 @@
 /*
  * @Date: 2022-02-21 08:48:11
  * @LastEditors: zhaoxm
- * @LastEditTime: 2022-05-04 21:00:23
+ * @LastEditTime: 2022-05-05 09:11:26
  * @Description: 更新版本
  */
-
-import chalk from "chalk"
+import { COMMIT_LINT_PACKAGE_NAME, ESLINT_ALL, ESLINT_VUE2, STYLE_LINT_PACKAGE_NAME } from "../config/const"
 import { failSpinner, startSpinner, succeedSpinier } from "./../utils/spinner"
+import { userPackage } from "../utils/userPackage"
+import chalk from "chalk"
 
 export default class Upgrade implements ACommands {
 
@@ -18,6 +19,16 @@ export default class Upgrade implements ACommands {
     try {
       startSpinner("正在更新。。。")
 
+      const packages = [ESLINT_VUE2,ESLINT_ALL,COMMIT_LINT_PACKAGE_NAME,STYLE_LINT_PACKAGE_NAME]
+
+      for(let pckName of packages) {
+        if(userPackage.dependenciesAndDevDependencies.includes(pckName)){
+          userPackage
+            .uninstall(pckName)
+            .install(pckName)
+        }
+      }
+
       succeedSpinier(chalk.green("更新成功!"))
 
     }
@@ -26,5 +37,4 @@ export default class Upgrade implements ACommands {
       return
     }
   }
-
 }
