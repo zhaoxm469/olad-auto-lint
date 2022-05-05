97d10776f658dc7c7b2044f55d21fa4bd98ec846
zhaoxingming <zhaoxingming@nucarf.com>
2022-05-05 09:23:48 +0800

    fix: 判断当前运行环境是否存在package.json文件

--- packages/lint-cli/src/cli.ts
+++ packages/lint-cli/src/cli.ts
@@ -2,6 +2,8 @@ import { version } from "../package.json"
 import { getCommandPaths } from "./utils/index"
 import { error } from "./utils/logger"
 import { program } from "commander"
+import { userPackage } from "./utils/userPackage"
+import { failSpinner } from "./utils/spinner"
 
 function init() {
   const commandPaths = getCommandPaths()
@@ -10,7 +12,12 @@ function init() {
     const CommandModule = require(commandPath).default
     const commandModule = new CommandModule()
     const { command, description, action } = commandModule
-    program.command(command).description(description).action(action.bind(commandModule))
+    program.command(command).description(description).action(()=>{
+      if(!userPackage.isHasPackageJson) {
+        return failSpinner('请确保在项目根目录下存在 package.json 文件')
+      }
+      return action(commandModule)
+    })
   })
 
   program.version(version)
