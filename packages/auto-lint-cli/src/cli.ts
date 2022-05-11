6c1e1ab27fd5d4c4400ff1026fdc10e7852d76b5
zhaoxingming <zhaoxingming@nucarf.com>
2022-05-11 23:20:45 +0800

    refactor: add errorLog

--- packages/auto-lint-cli/src/cli.ts
+++ packages/auto-lint-cli/src/cli.ts
@@ -1,16 +1,16 @@
+#!/usr/bin/env node
 import { version } from "../package.json"
 import { getCommandPaths } from "./utils/index"
-import { error } from "./utils/logger"
+import { errorLog } from "./utils/logger"
 import { program } from "commander"
 import { userPackage } from "./utils/userPackage"
 import { failSpinner } from "./utils/spinner"
 
 function init() {
+
   const commandPaths = getCommandPaths()
 
   for (const commandPath of commandPaths) {
-    console.log(commandPath)
-
     const CommandModule = require(commandPath).default
     const commandModule = new CommandModule()
     const { command, description, action } = commandModule
@@ -18,7 +18,10 @@ function init() {
       if (!userPackage.isHasPackageJson) {
         return failSpinner("请确保在项目根目录下存在 package.json 文件")
       }
-      return action(commandModule)
+      return action(commandModule).catch((error_: any) => {
+        errorLog(error_)
+        process.exit(1)
+      })
     })
   }
 
@@ -26,7 +29,7 @@ function init() {
 
   program.arguments("[command]").action((cmd) => {
     if (!cmd) return program.outputHelp()
-    error(`未知的命令${cmd}`)
+    errorLog(`未知的命令${cmd}`)
     program.outputHelp()
   })
 
