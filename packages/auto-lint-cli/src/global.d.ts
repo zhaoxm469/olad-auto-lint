8364485bd992ae0a08be847ca2b4ecc2b0fcd9d2
zhaoxingming <zhaoxingming@nucarf.com>
2022-05-11 23:20:54 +0800

    chore: 删除无用type

--- packages/auto-lint-cli/src/global.d.ts
+++ packages/auto-lint-cli/src/global.d.ts
@@ -1,26 +1,11 @@
 declare type Recordable<T = any> = Record<string, T>
 
-declare module "*.json"
-
 declare interface ACommands {
   command: string
   description: string
   action: (...arguments_: any[]) => any
 }
 
-declare type PackageJson = {
-  name: string
-  version: string
-  dependencies: Recordable
-  devDependencies: Recordable
-} & Recordable
-
-declare interface LintItem {
-  lintName: string
-  eslintType: string
-}
-
-declare interface Lints extends Array<LintItem> {}
 declare interface UserPackage {
   dependencies?: Recordable
   devDependencies?: Recordable
