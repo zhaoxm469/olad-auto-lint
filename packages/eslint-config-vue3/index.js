19a6f50f418974673839327607fc4add3700d077
zhaoxingming <zhaoxingming@nucarf.com>
2022-05-13 23:05:10 +0800

    chore: 修改Vue3规则

--- packages/eslint-config-vue3/index.js
+++ packages/eslint-config-vue3/index.js
@@ -1,3 +1,5 @@
+const rules = require("./rules")
+
 module.exports = {
   overrides: [
     {
@@ -22,40 +24,8 @@ module.exports = {
   ],
   extends: ["plugin:vue/vue3-recommended", "@olad/eslint-config-ts"],
   rules: {
-    "vue/no-multiple-slot-args": ["error"],
-    "vue/order-in-components": ["error"],
-    "vue/this-in-template": ["error"],
-    "vue/attribute-hyphenation": ["error"],
-    "vue/component-definition-name-casing": ["error"],
-    "vue/html-closing-bracket-newline": ["error"],
-    "vue/html-closing-bracket-spacing": ["error"],
-    "vue/html-end-tags": ["error"],
-    "vue/html-indent": ["error"],
-    "vue/html-quotes": ["error"],
-    "vue/multiline-html-element-content-newline": ["error"],
-    "vue/mustache-interpolation-spacing": ["error"],
-    "vue/no-multi-spaces": ["error"],
-    "vue/no-spaces-around-equal-signs-in-attribute": ["error"],
-    "vue/no-template-shadow": ["error"],
-    "vue/one-component-per-file": ["error"],
-    "vue/prop-name-casing": ["error"],
-    "vue/require-default-prop": ["error"],
+    ...rules,
     "vue/require-explicit-emits": ["error"],
-    "vue/require-prop-types": ["error"],
-    "vue/singleline-html-element-content-newline": ["error"],
-    "vue/v-bind-style": ["error"],
-    "vue/v-on-style": ["error"],
-    "vue/v-slot-style": ["error"],
-    "vue/attributes-order": "error",
-    // 一行代码属性最多个数
-    "vue/max-attributes-per-line": ["error", {
-      "singleline": {
-        "max": 1,
-      },
-      "multiline": {
-        "max": 1,
-      },
-    }],
     // 是否需要闭合标签
     "vue/html-self-closing": [
       "error",
