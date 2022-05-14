b8f14a04d253f7eecc80b3ae7bc23701dd228cd3
zhaoxingming <zhaoxingming@nucarf.com>
2022-05-15 02:20:01 +0800

    chore: 模块替换

--- packages/auto-lint-cli/src/cli.ts
+++ packages/auto-lint-cli/src/cli.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import { version } from "../package.json"
 import { getCommandFilePaths } from "./utils/index"
 import { errorLog, upgradeLog } from "./utils/logger"
-import { program } from "commander"
 import { userPackage } from "./utils/userPackage"
-import { failSpinner } from "./utils/spinner"
 import { promptConfig } from "./config/const"
+import { failSpinner } from "./utils/spinner"
+import { version } from "../package.json"
+import { program } from "commander"
 
 function init() {
 
@@ -20,10 +20,10 @@ function init() {
       if (!userPackage.isHasPackageJson) {
         return failSpinner("请确保在项目根目录下存在 package.json 文件")
       }
-      // return action(commandModule).catch((error_: any) => {
-      //   errorLog(error_)
-      //   process.exit(1)
-      // })
+      return action(commandModule).catch((error_: any) => {
+        errorLog(error_)
+        process.exit(1)
+      })
     })
       .hook("postAction", () => {
         upgradeLog(version)
